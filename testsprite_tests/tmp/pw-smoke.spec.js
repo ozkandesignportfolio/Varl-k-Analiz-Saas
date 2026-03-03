bcc877c8f514e3a83f86f4f6f15fb3b7224c133c
Osman Akan <ozkandesignportfolio@gmail.com>
2026-03-03 16:39:02 +0300

    chore: stop tracking testsprite tmp artifacts

--- testsprite_tests/tmp/pw-smoke.spec.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('playwright/test');
-
-test('smoke home', async ({ page }) => {
-  await page.goto('http://localhost:3000/login');
-  await expect(page).toHaveURL(/login/);
-});
